d708ca4229acb76060442568156c50715a85e86c
juugeebee <44608893+juugeebee@users.noreply.github.com>
2019-10-24 17:24:25 +0200

    erreur au niveau du 1er vsearch

--- 16S_workflow.sh
+++ 16S_workflow.sh
@@ -4,10 +4,11 @@ chmod -x 16S_workflow.sh
 folderin=$1
 folderout=$2
 
+mkdir $2
 mkdir $2/fastqc_avanttrim
 mkdir $2/fastqc_aprestrim
 mkdir $2/trimmed
-mkdir $2/vsearch
+mkdir $2/fasta
 
 #### Deziper les fastq
 gunzip $1/*.gz
@@ -34,49 +35,39 @@ do
 	fastqc $2/trimmed/*_R1.fastq.at.fq $2/trimmed/*_R2.fastq.at.fq -o $2/fastqc_aprestrim
 	
 	## Fusionner les reads à l’aide Vsearch > Pairer les séquence F et R > Merger
-	vsearch --fastq_mergepairs $2/trimmed/*_R1.fastq.at.fq --reverse $2/trimmed/*_R2.fastq.at.fq --fastaout fasta/$samplename.fasta --label_suffix "$s;sample=$samplename;" --fastq_minovlen 40 --fastq_maxdiffs 15
+	vsearch --fastq_mergepairs $2/trimmed/*_R1.fastq.at.fq --reverse $2/trimmed/*_R2.fastq.at.fq --fastaout $2/fasta/$samplename.fasta --label_suffix "$s;sample=$samplename;" --fastq_minovlen 40 --fastq_maxdiffs 15
 done
 
 
 #supprimer les espaces
 #mettre tout dans les fastas dans un fichier amplicon
-cat $2/vsearch/*.fasta > $2/vsearch/amplicon.fasta
-sed -i -e "s/ //g" $2/vsearch/amplicon.fasta
+cat $2/fasta/*.fasta > $2/fasta/amplicon.fasta
+sed -i -e "s/ //g" $2/fasta/amplicon.fasta
 
 
 #### Deduplication
 
 	## Full length
-soft/vsearch --derep_fulllength $2/vsearch/amplicon.fasta --log=vsearch_fl_log --sizeout --minuniquesize 10 --output $2/vsearch/amplicon_dedfl.fasta --uc $2/vsearch/amplicon_dedfl.uc
+soft/vsearch --derep_fulllength $2/fasta/amplicon.fasta --log=vsearch_fl_log --sizeout --minuniquesize 10 --output $2/fasta/amplicon_derep_fl.fasta --uc $2/fasta/amplicon_derep_fl.uc
 
 	## Prefix
-soft/vsearch --derep_prefix $2/vsearch/amplicon.fasta --log=vsearch_pref.log --sizeout --minuniquesize 10 --output $2/vsearch/amplicon_dedpref.fasta --uc $2/vsearch/amplicon_dedpref.uc
+soft/vsearch --derep_prefix $2/fasta/amplicon.fasta --log=vsearch_pref.log --sizeout --minuniquesize 10 --output $2/fasta/amplicon_derep_pref.fasta --uc $2/fasta/amplicon_derep_pref.uc
 
 
 #### Suppression des chimères
-soft/vsearch --uchime_denovo $2/vsearch/amplicon_dedfl.fasta --nonchimeras $2/vsearch/amplicon_dedfluchim.fasta --sizein --chimeras $2/vsearch/chimeras_fl.fasta
-
-soft/vsearch --uchime_denovo $2/vsearch/amplicon_dedpref.fasta --nonchimeras $2/vsearch/amplicon_dedprefuchim.fasta --sizein --chimeras $2/vsearch/chimeras_pref.fasta
+soft/vsearch --uchime_denovo $2/fasta/amplicon_derep_fl.fasta --nonchimeras $2/fasta/amplicon_non_chimeras.fasta --sizein --chimeras $2/fasta/chimeras.fasta
 
 
 #### clustering
-soft/vsearch --cluster_size $2/vsearch/amplicon_dedfluchim.fasta --id 0.97 --sizein --sizeout --relabel OTU_ --centroids $2/vsearch/amplicon_dedfluchim_OTU.fasta
-
-soft/vsearch --cluster_size $2/vsearch/amplicon_dedprefuchim.fasta --id 0.97 --sizein --sizeout --relabel OTU_ --centroids $2/vsearch/amplicon_dedprefuchim_OTU.fasta
+soft/vsearch --cluster_size $2/fasta/amplicon_non_chimeras.fasta --id 0.97 --sizein --sizeout --relabel OTU_ --centroids $2/fasta/OTU.fasta
 
 
 ### Déterminer l'abondance des OTUs
-soft/vsearch -usearch_global $2/vsearch/amplicon.fasta --otutabout $2/vsearch/dedfluchim_OTU  -db $2/vsearch/amplicon_dedfluchim_OTU.fasta --id 0.97
-
-
-soft/vsearch -usearch_global $2/vsearch/amplicon.fasta --otutabout $2/vsearch/dedprefuchim_OTU -db $2/vsearch/amplicon_dedprefuchim_OTU.fasta --id 0.97
+soft/vsearch -usearch_global $2/fasta/amplicon.fasta --otutabout $2/fasta/OTU  -db $2/fasta/amplicon_dedfluchim_OTU.fasta --id 0.97
 
 
 #### Annoter les OTU contre l’ensemble de 16S/18S fournis par le constructeur
-soft/vsearch --usearch_global $2/vsearch/amplicon_dedfluchim_OTU.fasta --userout  $2/vsearch/amplicon_dedfluchim_annot.fasta --db databases/mock_16S_18S.fasta  --id 0.9 --top_hits_only --userfields query+target
+soft/vsearch --usearch_global $2/fasta/OTU.fasta --userout  $2/fasta/annotations.fasta --db databases/mock_16S_18S.fasta  --id 0.9 --top_hits_only --userfields query+target
 
-soft/vsearch --usearch_global $2/vsearch/amplicon_dedprefuchim.fasta --userout  $2/vsearch/amplicon_dedprefuchim_annot.fasta --db databases/mock_16S_18S.fasta  --id 0.9 --top_hits_only --userfields query+target
-
-sed '1iOTU\tAnnotation' -i $2/vsearch/amplicon_dedfluchim_annot.fasta
-sed '1iOTU\tAnnotation' -i $2/vsearch/amplicon_dedprefuchim_annot.fasta
+sed '1iOTU\tAnnotation' -i $2/fasta/annotations.fasta
 
